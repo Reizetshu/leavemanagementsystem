3893412c1e7c44c503dbc45fa122164cb7054168
markbensonhernando <mbhernando27@gmail.com>
2025-06-20 16:29:09 +0800

    Feat: Adding comments for server.js and db.js for readability; adding basic routes on server.js for testing of the app.

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Import Mongoose for MongoDB connection
 
-const db = async () => {
+// Define the asynchronous function to connect to the database
+const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(
       `Database connected: ${connect.connection.host}, ${connect.connection.name}`
     );
   } catch (error) {
-    console.log(error);
+    // Log any errors that occur during connection
+    console.log(`Error: ${error.message}`);
+    // Exit the process with a failure code
     process.exit(1);
   }
 };
 
-module.exports = db;
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const db = require('./config/db');
+const express = require('express'); // Import the Express.js framework
+const dotenv = require('dotenv').config(); // Import dotenv to load environment variables from .env file
+const connectDB = require('./config/db'); // Import the database connection function
 
-db();
+// Connect to the MongoDB database
+connectDB();
 
+// Initialize the Express Application
 const app = express();
 
-// Middleware
+// Middleware to parse JSON request (for handling incoming request bodies)
 app.use(express.json());
 
-// Routes
+// Basic route to test if the server is running
+app.get('/', (req, res) => {
+  res.send('App is running...');
+});
 
-// Start the server
+// Define the port the server will listen on.
+// Use the PORT from environment variables, or default to 5000 if not set.
 const PORT = process.env.PORT || 5000;
+
+// Start the server and listen for incoming requests
 app.listen(PORT, () => {
   console.log(`Server is running in port ${PORT}`);
 });
